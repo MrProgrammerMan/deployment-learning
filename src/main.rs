dd2de83fefe8cf955926d54f96b2ee499774845a
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-06-23 20:08:20 +0200

    feat(static): add templated html files

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,33 @@ use axum::{
     routing::get,
     Router
 };
+use askama::{
+    Template
+};
+use axum::response::Html;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct HomeTemplate {}
+
+#[derive(Template)]
+#[template(path = "quotes.html")]
+struct QuotesTemplate {}
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(home_handler));
+        .route("/", get(home_handler))
+        .route("/quotes", get(quotes_handler));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn home_handler() -> String {
-    "The Home Page".to_string()
+async fn home_handler() -> Html<String> {
+    Html(HomeTemplate{}.render().unwrap())
+}
+
+async fn quotes_handler() -> Html<String> {
+    Html(QuotesTemplate{}.render().unwrap())
 }
